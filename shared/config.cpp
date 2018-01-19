efbee5aa15b594e5d819b2d322bc2e0a55ab4099
Craig Richards <craigr3@hotmail.com>
2018-01-19 21:33:01 +0000

    Added a base configuration class to handle reading config items

---
+++ shared/config.cpp
@@ -0,0 +1,117 @@
+#include "../shared/config.h"
+#include "../shared/strutils.h"
+#include <string.h>
+#include <stdlib.h>
+
+Config::Config(const char* fileName)
+{
+	openFile(fileName);
+}
+
+
+Config::~Config()
+{
+	closeFile();
+}
+
+bool Config::ReadItem(const char* key, char* value, unsigned int valueSize)
+{
+	if (!m_file)
+		return false;
+
+	rewind(m_file);
+
+	static const char* tok = "\t = \r\n";
+
+	char line[32] = { 0 };
+	char* pch, *configKey;
+	while (fgets(line, sizeof(line), m_file))
+	{
+		// Is the first character a #? Its probably a comment
+		if (*line == '#')
+			continue;
+
+		// Is the line empty or a new line?
+		if ((!*line) || (*line == '\r') || (*line == '\n'))
+			continue;
+
+		pch = (char*)strtok(line, tok);
+		if (pch)
+		{
+			configKey = pch;
+			if (((pch = (char*)strtok(0, tok)) == 0) || (*pch == '\n'))
+				continue;
+
+			if (str_equal_nocase(configKey, key))
+			{
+				str_cpy(value, pch, valueSize);
+				return 0;
+			}
+		}
+	}
+
+	return false;
+}
+
+bool Config::ReadItemInt(const char* key, int* value)
+{
+	if (!m_file)
+		return false;
+
+	rewind(m_file);
+
+	static const char* tok = "\t = \r\n";
+
+	char line[32] = { 0 };
+	char* pch, *configKey;
+	while (fgets(line, sizeof(line), m_file))
+	{
+		// Is the first character a #? Its probably a comment
+		if (*line == '#')
+			continue;
+
+		// Is the line empty or a new line?
+		if ((!*line) || (*line == '\r') || (*line == '\n'))
+			continue;
+
+		pch = (char*)strtok(line, tok);
+		if (pch)
+		{
+			configKey = pch;
+			if (((pch = (char*)strtok(0, tok)) == 0) || (*pch == '\n'))
+				continue;
+
+			if (str_equal_nocase(configKey, key))
+			{
+				if (isnum(pch))
+				{
+					*value = atoi(pch);
+					return 0;
+				}
+			}
+		}
+	}
+
+	return false;
+}
+
+bool Config::openFile(const char* fileName)
+{
+	FILE* tmp = fopen(fileName, "a+");
+	if (tmp)
+	{
+		m_file = tmp;
+
+		return true;
+	}
+	return false;
+}
+
+void Config::closeFile()
+{
+	if (m_file)
+	{
+		fclose(m_file);
+		m_file = nullptr;
+	}
+}
---
+++ shared/config.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "ISingleton.h"
+#include <stdio.h>
+
+class Config : public ISingleton<Config>
+{
+public:
+	Config(const char* fileName);
+	~Config();
+
+	bool ReadItem(const char* key, char* value, unsigned int valueSize);
+	bool ReadItemInt(const char* key, unsigned int* value);
+
+private:
+	bool openFile(const char* fileName);
+	void closeFile();
+
+private:
+	FILE * m_file;
+};
+
